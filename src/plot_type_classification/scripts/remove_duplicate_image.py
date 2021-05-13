12a88e63504adba45876e4966d27c19358b8df49
TURBOPC\ursau <uz2273@student.uni-lj.si>
2021-05-13 20:03:47 +0200

    removing duplicates in all folders

--- src/plot_type_classification/scripts/remove_duplicate_image.py
+++ src/plot_type_classification/scripts/remove_duplicate_image.py
@@ -3,9 +3,10 @@ import cv2
 import itertools
 import numpy as np
 
+
 def remove_duplicates(folder):
     images = {os.path.join(folder, filename): cv2.imread(os.path.join(folder, filename), cv2.IMREAD_GRAYSCALE) for filename
-              in os.listdir(folder)}
+              in os.listdir(folder) if not os.path.isdir(os.path.join(folder, filename))}
 
     all_groups = [list(grouper) for _, grouper in itertools.groupby(list(images.items()), key=lambda x: x[1].shape)]
 
@@ -16,14 +17,28 @@ def remove_duplicates(folder):
             if img_diff_sum == 0:
                 duplicates.add(path2)
 
-    print(f"Found {len(duplicates)} duplicates. Removing...")
+    if len(duplicates) == 0:
+        print(f"Found 0 duplicates.")
+    else:
+        print(f"Found {len(duplicates)} duplicates. Removing...")
 
     for el in duplicates:
         os.remove(el)
+        # print(el)
+
 
 if __name__ == '__main__':
 
-    path = "../data/remove_dups_test"
-    remove_duplicates(path)
+    start_path = "D:/project/filtered"
+
+    for f in ['ef', 'fmf', 'fri']:
+        for t in ['bachelor', 'master', 'other', 'phd']:
+            for s in ['almost_sure', 'sure']:
+                for c in os.listdir(os.path.join(start_path, f, t, s)):
+                    path = os.path.join(start_path, f, t, s, c)
+                    print(f'Currently at {path}.')
+                    remove_duplicates(path)
+
+            remove_duplicates(os.path.join(start_path, f, t, 'not_sure'))
 
 
